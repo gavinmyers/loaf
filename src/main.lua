27c48698f6ddaaa6010147f24075126d9161d45a
gavin myers <gavin.myers@quantumretail.com>
2014-05-17 10:45:29 -0500

    rebuilding

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,12 @@
 require "shader/lighting"
 require "map"
+require "graphics"
 
 area = nil
 function love.load()
+  graphics.init()
   area = map.create()
-  print(area.tiles[20][20])
+  
 end
 
 function love.update(dt)
@@ -12,5 +14,11 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.print("loaf",0,0)
+  love.graphics.print("loaf " .. area.tiles[0][0],0,0)
+  local f1 = graphics.sprites["floor001"]
+  local f2 = graphics.sprites["floor002"]
+  local p = graphics.sprites["player001"]
+  love.graphics.draw(f1.sprite, f1.quad, 200, 200)
+  love.graphics.draw(f2.sprite, f2.quad, 200, 216)
+  love.graphics.draw(p.sprite, p.quad, 200, 200)
 end
