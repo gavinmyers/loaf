ce76fe6d4b80591ba9b4ffebfd06cfcdb1e4a1aa
gavin myers <gavin.myers@quantumretail.com>
2014-08-10 22:51:21 -0500

    you can no kill a goblin (not as cool as it sounds)

--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,7 @@
 floorTiles,wallTiles,gameTiles,playerTiles = nil
 currentMap = nil
 player = nil
+mode = "MOVE" 
 
 math.randomseed(os.time())
 local inspect = require "lib/inspect"
@@ -28,9 +29,28 @@ win.w = tile.sz * (tile.acs + 2)
 win.h = tile.sz * (tile.dwn + 2) 
 
 player = {}
+player.hp = 100
+player.attack = 2
+player.defend = 2
+player.damage = 2
 player.x = 1
 player.y = 1
 player.tile = nil
+player.die = function()
+end
+
+goblin = {}
+goblin.hp = 50
+goblin.attack = 1
+goblin.defend = 2
+goblin.damage = 2
+goblin.x = 1
+goblin.y = 1
+goblin.tile = nil
+goblin.die = function()
+  map.creatures[goblin.x][goblin.y] = nil 
+  print("SQUEEEEE!")
+end
 
 
 function main()
@@ -47,11 +67,11 @@ function main()
   map = {}
   map.floor = {}
   map.structure = {}
-  map.things = {}
+  map.creatures = {}
   for x = 1, tile.acs do
     map.floor[x] = {}
     map.structure[x] = {}
-    map.things[x] = {}
+    map.creatures[x] = {}
   end
   currentMap = generators.simple() 
   map.structure = currentMap.map
@@ -63,23 +83,41 @@ function main()
   player.y = currentMap.startY
   player.tile = playerTiles[1]
 
-  map.things[player.x][player.y] = player.tile 
+  goblin.x = currentMap.endX
+  goblin.y = currentMap.endY
+  goblin.tile = playerTiles[2]
 
-  map.things[currentMap.endX][currentMap.endY] = playerTiles[2] 
+  map.creatures[player.x][player.y] = player
+  map.creatures[goblin.x][goblin.y] = goblin 
 end
 
 function action(who,targetX,targetY) 
   if targetX < 2 or targetX > tile.acs -1 then 
   elseif targetY < 2 or targetY > tile.dwn -1 then
-  elseif map.structure[targetX][targetY] ~= nil then
-  else
-    map.things[player.x][player.y] = nil 
-    player.x = targetX
-    player.y = targetY
-    map.things[player.x][player.y] = player.tile 
-    if player.x == currentMap.endX and player.y == currentMap.endY then
-      main()
+  elseif mode == "MOVE" then
+    if map.structure[targetX][targetY] ~= nil then
+    elseif map.creatures[targetX][targetY] ~= nil then
+    else
+      map.creatures[who.x][who.y] = nil 
+      who.x = targetX
+      who.y = targetY
+      map.creatures[who.x][who.y] = who
+      if who.x == currentMap.endX and who.y == currentMap.endY then
+        main()
+      end
     end
+  elseif mode == "PRIMARY" then
+    if map.creatures[targetX][targetY] ~= nil then
+      local attacker = who
+      local defender = map.creatures[targetX][targetY]
+      if math.random(1,attacker.attack * 6) > math.random(1, defender.defend * 6) then
+        defender.hp = defender.hp - math.random(1,attacker.damage * 6)
+        if defender.hp < 1 then
+          defender.die()
+        end
+      end
+    end
+    mode = "MOVE" 
   end
 end
 
@@ -98,6 +136,10 @@ function love.keypressed(key)
     action(player, player.x, player.y+1)
   elseif key == "d" then
     action(player, player.x+1, player.y)
+  elseif key == "q" then
+    mode = "PRIMARY"
+  elseif key == "e" then
+    mode = "MOVE"
   elseif key == " " then
     main()
   end
@@ -116,8 +158,8 @@ function love.draw()
       if map.structure[x] ~= nil and map.structure[x][y] ~= nil then
         tile.graphics.draw(map.structure[x][y],x,y)
       end
-      if map.things[x] ~= nil and map.things[x][y] ~= nil then
-        tile.graphics.draw(map.things[x][y],x,y)
+      if map.creatures[x] ~= nil and map.creatures[x][y] ~= nil then
+        tile.graphics.draw(map.creatures[x][y].tile,x,y)
       end
     end
   end
